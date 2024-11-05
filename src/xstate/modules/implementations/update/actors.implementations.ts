9a0377b51bde4b049b9afd69e204c82ca26e8b2c
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2024-11-05 20:09:03 +0800

    fix update actor

--- src/xstate/modules/implementations/update/actors.implementations.ts
+++ src/xstate/modules/implementations/update/actors.implementations.ts
@@ -4,6 +4,7 @@ import { fromPromise } from 'xstate';
 import { IActors } from '../../schemas/update/update.schema';
 import { Utility } from 'src/utils/utility.service';
 import { SyncService } from '@dna-platform/crdt-lww';
+import { pick } from 'lodash';
 @Injectable()
 export class UpdateActorsImplementations {
   constructor(private readonly syncService: SyncService) {}
@@ -23,11 +24,15 @@ export class UpdateActorsImplementations {
           },
         });
       const [_res, _req] = context?.controller_args;
-      const { params, body } = _req;
-      const { meta, data } = body;
+      const { params, body, query } = _req;
       const { table, id } = params;
-      const { schema } = Utility.checkCreateSchema(table, meta, data);
-      const updated_data = Utility.updateParse({ schema, data });
+      const { pluck = 'id' } = query;
+      const { schema } = Utility.checkCreateSchema(
+        table,
+        undefined as any,
+        body,
+      );
+      const updated_data = Utility.updateParse({ schema, data: body });
       delete updated_data.id;
       const result = await this.syncService.update(table, updated_data, id);
       return Promise.resolve({
@@ -35,7 +40,7 @@ export class UpdateActorsImplementations {
           success: true,
           message: `Successfully updated in ${table}`,
           count: 1,
-          data: [result],
+          data: [pick(result, pluck.split(','))],
         },
       });
     }),
