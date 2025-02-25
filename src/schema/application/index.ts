f4ace6f6f36709c59d8adf28099c9b480a6b3379
Kashan Ali Khalid <kashankhalid@dnamicro.com>
2025-02-25 16:55:54 +0100

    wallguard logs table added

--- src/schema/application/index.ts
+++ src/schema/application/index.ts
@@ -4,3 +4,4 @@ export { table as files } from '../application/files';
 export { table as packets } from '../application/packets'; //hypertable
 export { table as temp_packets } from '../application/temp_packets';
 export { table as dead_letter_queue } from '../application/dead_letter_queue';
+export { table as wallguard_logs } from '../application/wallguard_logs';
