cd3f95885af935ce57bbf04225f6e425b0df7910
Kashan Ali Khalid <kashankhalid@dnamicro.com>
2025-02-28 18:15:07 +0100

    added error handling for wrong field names in sort

--- src/xstate/modules/implementations/find/actors.implementations.ts
+++ src/xstate/modules/implementations/find/actors.implementations.ts
@@ -8,11 +8,11 @@ import { fromPromise } from 'xstate';
 import { IActors } from '../../schemas/find/find.schema';
 import { DrizzleService } from '@dna-platform/crdt-lww-postgres';
 import { Utility } from '../../../../utils/utility.service';
-
+import * as local_schema from '../../../../schema';
 import { asc, desc, sql, SQLWrapper, AnyColumn } from 'drizzle-orm';
 import { VerifyActorsImplementations } from '../verify';
 import { IParsedConcatenatedFields } from '../../../../types/utility.types';
-const pluralize = require('pluralize');
+// const pluralize = require('pluralize');
 @Injectable()
 export class FindActorsImplementations {
   private db;
@@ -55,6 +55,18 @@ export class FindActorsImplementations {
         date_format = 'YYYY-MM-DD',
         // pluck_group_object = {},
       } = body;
+      const parsed_concatenated_fields =
+        Utility.parseConcatenateFields(concatenate_fields);
+
+      let aliased_joined_entities: Record<string, any>[] = [];
+      joins.forEach(({ field_relation }) => {
+        const { entity, alias } = field_relation.to;
+        if (alias) {
+          aliased_joined_entities.push({ alias, entity });
+        }
+      });
+
+      console.log(aliased_joined_entities);
       Object.keys(pluck_object).forEach((key) => {
         if (!pluck_object[key].includes('id')) {
           throw new BadRequestException({
@@ -62,6 +74,42 @@ export class FindActorsImplementations {
             message: `pluck_object must have "id" for every entity`,
           });
         }
+      });
+
+      multiple_sort.forEach(({ by_field }) => {
+        const [entity, field] = by_field.split('.');
+        const concat_fields = parsed_concatenated_fields.fields;
+        const non_aliased_entity: string =
+          aliased_joined_entities.find(({ alias }) => alias === entity)
+            ?.entity || entity;
+        console.log(non_aliased_entity);
+        const field_exists =
+          local_schema[non_aliased_entity]?.[field] ||
+          concat_fields[entity]?.find((exp) => exp.includes(field));
+        if (!field_exists) {
+          let message;
+          if (non_aliased_entity === entity) {
+            message = `Field ${field} does not exist in ${entity}`;
+          } else {
+            message = `Field ${field} does not exist in ${entity} which is alias of ${non_aliased_entity}`;
+          }
+          throw new BadRequestException({
+            success: false,
+            message,
+          });
+        }
+        const concat = concatenate_fields.find(
+          (concat_entity) =>
+            concat_entity.field_name === field &&
+            concat_entity.entity === entity,
+        );
+
+        pluck_object[entity] = [
+          ...new Set([
+            ...pluck_object[entity],
+            ...(concat ? concat.fields : [field]),
+          ]),
+        ];
       });
       let _pluck: string[] =
         pluck.length && !pluck.includes('*') ? pluck : ['id', 'code'];
@@ -79,9 +127,6 @@ export class FindActorsImplementations {
       let _db = this.db;
 
       // let join_keys: string[] = Object.keys(pluck_object);
-      let aliased_joined_entities: Record<string, any>[] = [];
-      const parsed_concatenated_fields =
-        Utility.parseConcatenateFields(concatenate_fields);
 
       if (joins?.length) {
         _db = _db
@@ -124,7 +169,7 @@ export class FindActorsImplementations {
           const is_aliased = Object.values(aliased_entities).find(
             ({ alias }) => alias === _entity,
           );
-          const entity = !is_aliased ? pluralize(_entity) : _entity;
+          const entity = _entity;
           // if (!join_keys.includes(entity) && !is_aliased)
           //   throw new BadRequestException({
           //     success: false,
@@ -132,9 +177,9 @@ export class FindActorsImplementations {
           //   });
           if (
             !schema[entity]?.[by_field] &&
-            !is_aliased &&
             transformed_concatenations[sort_entity] &&
             sort_entity === table
+            //if sort_entity is the main table and check if it has any field that is concatenated, and that field doesn't exist in the schema
           ) {
             const concatenation = transformed_concatenations[entity]?.find(
               (exp) => exp.includes(by_field),
@@ -144,30 +189,25 @@ export class FindActorsImplementations {
               : undefined;
           } else if (
             !schema[entity]?.[by_field] &&
-            !is_aliased &&
             transformed_concatenations[sort_entity] &&
             transformed_concatenations[sort_entity].find((exp) =>
               exp.includes(by_field),
             )
+            //if entity is not in the schema or its field is not in the schema and it is in the transformed concatenations and the field is in the transformed concatenations
           ) {
             const concat_sort_field: any = transformed_concatenations[
               sort_entity
             ]?.find((exp) => {
               return exp.includes(by_field);
             });
-            const sort_query = concat_sort_field
-              .replaceAll('joined_', '')
-              .split(' AS ')[0];
+            const sort_query = concat_sort_field.split(' AS ')[0];
 
             if (by_direction.toLowerCase() === 'asc') {
               return sql.raw(`MIN(${sort_query})`);
             } else {
               return sql.raw(`MAX(${sort_query})`);
             }
-          } else if (
-            (!schema[entity]?.[by_field] && !is_aliased) ||
-            entity !== table
-          ) {
+          } else if (entity !== table) {
             let sort_query: any = `"${sort_entity}"."${by_field}"`;
             if (by_direction.toLowerCase() === 'asc') {
               return sql.raw(`MIN(${sort_query})`);
