694a9aba1932e9b2107489e926d03b9c65130fe2
Kashan Ali Khalid <kashankhalid@dnamicro.com>
2025-03-07 00:57:19 +0100

    change chunk interval size to 1 day

--- src/schema/create_hypertables.ts
+++ src/schema/create_hypertables.ts
@@ -112,7 +112,7 @@ function writeHypertableQueriesToDrizzleFile(tableNames: string[]) {
     const queries = tableNames
       .map(
         (tableName) =>
-          `SELECT create_hypertable('${tableName}', 'timestamp', chunk_time_interval => INTERVAL '7 days', if_not_exists => TRUE);`,
+          `SELECT create_hypertable('${tableName}', 'timestamp', chunk_time_interval => INTERVAL '1 day', if_not_exists => TRUE);`,
       )
       .join('\n');
 
