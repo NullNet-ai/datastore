7b49d47bd1c22003697d478734a22b4742ad715e
GyulyVGC <gyulyvgc99@gmail.com>
2025-03-24 14:55:51 +0100

    update method Table::upsert_record

--- bin/store/src/table_enum.rs
+++ bin/store/src/table_enum.rs
@@ -1,10 +1,14 @@
 use crate::schema::schema;
+use diesel::associations::HasTable;
 use diesel::prelude::*;
 use diesel::query_builder::InsertStatement;
 use diesel::query_dsl::LoadQuery;
 use diesel::result::Error as DieselError;
 use serde::Serialize;
 use serde_json;
+use serde_json::Value;
+use crate::models::packet_model::Packet;
+use crate::schema::schema::packets::dsl::packets;
 
 #[derive(Debug)]
 pub enum Table {
@@ -45,35 +49,25 @@ impl Table {
         Ok(serde_json::to_string(&result).unwrap_or_else(|_| "{}".to_string()))
     }
 
-    pub fn upsert_record<'a, T, M, U>(
+    pub fn upsert_record(
         &self,
         conn: &mut PgConnection,
-        table: T,
-        record: M,
-    ) -> Result<String, DieselError>
-    where
-        T: diesel::Table + Clone,
-        M: diesel::Insertable<T>,
-        U: Serialize,
-        InsertStatement<T, M::Values>: LoadQuery<'a, PgConnection, U>,
-    {
-        diesel::insert_into(table.clone())
-            .values(record)
-            .on_conflict(schema::packets::id)
-            .do_update()
-            .set(table)
-            .execute(conn)
-            .map(|_| ());
-        // let result = diesel::insert_into(table)
-        //     .values(record)
-        //     .get_result::<U>(conn)?;
-
-        // Convert the result to a JSON string
-
-
-        Ok(serde_json::to_string(&result).unwrap_or_else(|_| "{}".to_string()))
+        record: Value,
+    ) -> Result<(), DieselError> {
+        match self {
+            Table::Packets => {
+                let value: Packet = serde_json::from_value(record).map_err(|e| {
+                    DieselError::DeserializationError(Box::new(e))
+                })?;
+                diesel::insert_into(packets::table())
+                    .values(value.clone())
+                    .on_conflict(schema::packets::id)
+                    .do_update()
+                    .set(value)
+                    .execute(conn)
+                    .map(|_| ())
+            }
+            _ => panic!(),
+        }
     }
 }
-
-
-
