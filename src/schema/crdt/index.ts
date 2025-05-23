3459b4318f8b9131885e5668ae87fb6380d7e7ab
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2025-05-24 06:16:45 +0800

    working integration for template using module @dna-platform/crdt-lww-postgres@1.0.119

--- src/schema/crdt/index.ts
+++ src/schema/crdt/index.ts
@@ -1,26 +1,34 @@
-export * from '@dna-platform/crdt-lww-postgres/build/schema';
+// export * from '@dna-platform/crdt-lww-postgres/build/schema';
 
-// export {
-//   //sync
-//   config_sync,
-//   merkles,
-//   messages,
-//   queues,
-//   queue_items,
-//   sync_endpoints,
-//   transactions,
-//   // system application
-//   contact_emails,
-//   contact_phone_numbers,
-//   // contacts,
-//   organization_contact_accounts,
-//   organization_contacts,
-//   organization_domains,
-//   organization_files,
-//   organizations,
-//   config_applications,
-//   // global config
-//   class_types,
-//   fields,
-//   allowed_fields,
-// } from '@dna-platform/crdt-lww/build/schema';
+export {
+  //sync
+  addresses,
+  devices,
+  config_sync,
+  merkles,
+  messages,
+  queues,
+  queue_items,
+  sync_endpoints,
+  transactions,
+  // system application
+  contact_emails,
+  contact_phone_numbers,
+  contacts,
+  //   organization_contact_accounts,
+  organization_contacts,
+  organization_accounts,
+  organization_domains,
+  organization_files,
+  organizations,
+  config_applications,
+  // global config
+  class_types,
+  fields,
+  allowed_fields,
+  counters,
+  external_contacts,
+  account_organizations,
+  accounts,
+  account_profiles,
+} from '@dna-platform/crdt-lww-postgres/build/schema';
