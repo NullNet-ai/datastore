d271c55ffeb3a0adb2e44f33dadd92b13be5c1fe
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2025-08-05 06:16:37 +0800

    revise field name in files schema

--- bin/store/src/schema/tables/files.rs
+++ bin/store/src/schema/tables/files.rs
@@ -35,7 +35,7 @@ define_table_schema! {
         version_id: nullable(text()), 
         download_path: nullable(text()), 
         presigned_url: nullable(text()), 
-        presigned_url_expires: nullable(integer()),
+        presigned_url_expire: nullable(integer()),
     },
     indexes: {
         // System field indexes
