550130ab735421eae38fcfb1d0ba06b5402d194b
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2025-08-12 01:46:38 +0800

    initial fixes

--- bin/store/src/providers/find/sql_constructor.rs
+++ bin/store/src/providers/find/sql_constructor.rs
@@ -1,7 +1,10 @@
-use crate::{schema::hypertables::is_hypertable, structs::structs::{
-    ConcatenateField, FilterCriteria, FilterOperator, GetByFilter, GroupAdvanceFilter, GroupBy,
-    Join, LogicalOperator, MatchPattern, SortOption,
-}};
+use crate::{
+    schema::hypertables::is_hypertable,
+    structs::structs::{
+        ConcatenateField, FilterCriteria, FilterOperator, GetByFilter, GroupAdvanceFilter, GroupBy,
+        Join, LogicalOperator, MatchPattern, SortOption,
+    },
+};
 use std::collections::HashMap;
 // Trait to define common interface for both GetByFilter and AggregationFilter
 pub trait QueryFilter {
@@ -223,7 +226,13 @@ impl<T: QueryFilter> SQLConstructor<T> {
                         .map(|f| {
                             format!(
                                 "COALESCE({}, '')",
-                                Self::get_field_with_parse_as(table, f, format_str, None, self.table.as_str())
+                                Self::get_field_with_parse_as(
+                                    table,
+                                    f,
+                                    format_str,
+                                    None,
+                                    self.table.as_str()
+                                )
                             )
                         })
                         .collect::<Vec<_>>()
@@ -272,7 +281,10 @@ impl<T: QueryFilter> SQLConstructor<T> {
         } else {
             field.to_string()
         };
-        format!("({}::time {})::time AS {}", field, timezone_query, field_name)
+        format!(
+            "({}::time {})::time AS {}",
+            field, timezone_query, field_name
+        )
     }
     fn construct_selections(&self) -> String {
         let mut selections = Vec::new();
@@ -317,7 +329,13 @@ impl<T: QueryFilter> SQLConstructor<T> {
                         .map(|f| {
                             format!(
                                 "COALESCE({}, '')",
-                                Self::get_field_with_parse_as(table_name, f, self.request_body.get_date_format(), None, self.table.as_str())
+                                Self::get_field_with_parse_as(
+                                    table_name,
+                                    f,
+                                    self.request_body.get_date_format(),
+                                    None,
+                                    self.table.as_str()
+                                )
                             )
                         })
                         .collect::<Vec<_>>()
@@ -403,8 +421,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
 
             // Add unique fields from group_by.fields
             for field in &group_by.fields {
-                let field_selection =
-                    Self::get_field(&self.table, field, self.request_body.get_date_format(), self.table.as_str());
+                let field_selection = Self::get_field(
+                    &self.table,
+                    field,
+                    self.request_body.get_date_format(),
+                    self.table.as_str(),
+                );
                 selections.push(field_selection);
             }
         }
@@ -446,7 +468,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
                         .map(|f| {
                             format!(
                                 "COALESCE({}, '')",
-                                Self::get_field(table_name, f, self.request_body.get_date_format(), self.table.as_str())
+                                Self::get_field(
+                                    table_name,
+                                    f,
+                                    self.request_body.get_date_format(),
+                                    self.table.as_str()
+                                )
                             )
                         })
                         .collect::<Vec<_>>()
@@ -547,7 +574,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
                     .iter()
                     .filter(|field| *field != "id" && !concatenated_field_names.contains(field))
                     .map(|field| {
-                        Self::get_field(&self.table, field, self.request_body.get_date_format(), self.table.as_str())
+                        Self::get_field(
+                            &self.table,
+                            field,
+                            self.request_body.get_date_format(),
+                            self.table.as_str(),
+                        )
                     }),
             );
         }
@@ -624,7 +656,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
                 format!(
                     "'{}', {}",
                     field,
-                    Self::get_field(to_alias, field, self.request_body.get_date_format(), self.table.as_str())
+                    Self::get_field(
+                        to_alias,
+                        field,
+                        self.request_body.get_date_format(),
+                        self.table.as_str()
+                    )
                 )
             })
             .collect()
@@ -650,7 +687,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
                         .map(|f| {
                             format!(
                                 "COALESCE({}, '')",
-                                Self::get_field(table_name, f, self.request_body.get_date_format(), self.table.as_str())
+                                Self::get_field(
+                                    table_name,
+                                    f,
+                                    self.request_body.get_date_format(),
+                                    self.table.as_str()
+                                )
                             )
                         })
                         .collect::<Vec<_>>()
@@ -767,8 +809,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
                 return String::new();
             }
 
-            let field_expression =
-                Self::get_field(table_alias, order_by, self.request_body.get_date_format(), &self.table);
+            let field_expression = Self::get_field(
+                table_alias,
+                order_by,
+                self.request_body.get_date_format(),
+                &self.table,
+            );
 
             // Handle case sensitivity
             let final_field = if self
@@ -1201,6 +1247,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
         case_sensitive: Option<bool>,
         match_pattern: Option<&MatchPattern>,
     ) -> String {
+        let mut parts = field_name.split(".");
+        let table_name = parts.next().unwrap_or("").replace("\"", "");
+        let field_name = parts.next().unwrap_or("").replace("\"", "");
+        let field_with_table = format!("{}.{}", table_name, field_name);
+        let plural_form = pluralizer::pluralize(&field_name, 2, false);
+        let is_plural = plural_form == field_name;
         let values_str = values
             .iter()
             .map(|v| match v {
@@ -1208,45 +1260,55 @@ impl<T: QueryFilter> SQLConstructor<T> {
                 serde_json::Value::Number(n) => n.to_string(),
                 serde_json::Value::Bool(b) => b.to_string(),
                 serde_json::Value::Null => "NULL".to_string(),
-                _ => format!("'{}'", v.to_string().replace("'", "''")),
+                _ => format!("'{}'", v.to_string().trim().replace("'", "''")),
             })
             .collect::<Vec<_>>();
 
         match operator {
             FilterOperator::Equal => {
                 if values_str.len() == 1 {
-                    format!("{} = {}", field_name, values_str[0])
+                    format!("{} = {}", field_with_table, values_str[0])
                 } else {
-                    format!("{} IN ({})", field_name, values_str.join(", "))
+                    format!("{} IN ({})", field_with_table, values_str.join(", "))
                 }
             }
             FilterOperator::NotEqual => {
                 if values_str.len() == 1 {
-                    format!("{} != {}", field_name, values_str[0])
+                    format!("{} != {}", field_with_table, values_str[0])
                 } else {
                     // Use AND for each item: field != value1 AND field != value2 AND ...
                     let conditions: Vec<String> = values_str
                         .iter()
-                        .map(|value| format!("{} != {}", field_name, value))
+                        .map(|value| format!("{} != {}", field_with_table, value))
                         .collect();
                     format!("({})", conditions.join(" AND "))
                 }
             }
-            FilterOperator::GreaterThan => format!("{} > {}", field_name, values_str[0]),
-            FilterOperator::GreaterThanOrEqual => format!("{} >= {}", field_name, values_str[0]),
-            FilterOperator::LessThan => format!("{} < {}", field_name, values_str[0]),
-            FilterOperator::LessThanOrEqual => format!("{} <= {}", field_name, values_str[0]),
-            FilterOperator::IsNull => format!("{} IS NULL", field_name),
-            FilterOperator::IsNotNull => format!("{} IS NOT NULL", field_name),
+            FilterOperator::GreaterThan => format!("{} > {}", field_with_table, values_str[0]),
+            FilterOperator::GreaterThanOrEqual => format!("{} >= {}", field_with_table, values_str[0]),
+            FilterOperator::LessThan => format!("{} < {}", field_with_table, values_str[0]),
+            FilterOperator::LessThanOrEqual => format!("{} <= {}", field_with_table, values_str[0]),
+            FilterOperator::IsNull => format!("{} IS NULL", field_with_table),
+            FilterOperator::IsNotNull => format!("{} IS NOT NULL", field_with_table),
             FilterOperator::Contains => {
                 let like_op = if case_sensitive.unwrap_or(true) {
                     "LIKE"
                 } else {
                     "ILIKE"
                 };
+
+                if is_plural {
+                    return format!(
+                        "{}::text {} '%{}%'",
+                        field_with_table,
+                        like_op,
+                        values_str[0].trim_matches('\'')
+                    );
+                }
+
                 format!(
                     "{} {} '%{}%'",
-                    field_name,
+                    field_with_table,
                     like_op,
                     values_str[0].trim_matches('\'')
                 )
@@ -1257,9 +1319,19 @@ impl<T: QueryFilter> SQLConstructor<T> {
                 } else {
                     "NOT ILIKE"
                 };
+
+                if is_plural {
+                    return format!(
+                        "{}::text {} '%{}%'",
+                        field_with_table,
+                        like_op,
+                        values_str[0].trim_matches('\'')
+                    );
+                }
+
                 format!(
                     "{} {} '%{}%'",
-                    field_name,
+                    field_with_table,
                     like_op,
                     values_str[0].trim_matches('\'')
                 )
@@ -1268,24 +1340,24 @@ impl<T: QueryFilter> SQLConstructor<T> {
                 if values_str.len() >= 2 {
                     format!(
                         "{} BETWEEN {} AND {}",
-                        field_name, values_str[0], values_str[1]
+                        field_with_table, values_str[0], values_str[1]
                     )
                 } else {
-                    format!("{} = {}", field_name, values_str[0])
+                    format!("{} = {}", field_with_table, values_str[0])
                 }
             }
             FilterOperator::IsNotBetween => {
                 if values_str.len() >= 2 {
                     format!(
                         "{} NOT BETWEEN {} AND {}",
-                        field_name, values_str[0], values_str[1]
+                        field_with_table, values_str[0], values_str[1]
                     )
                 } else {
-                    format!("{} != {}", field_name, values_str[0])
+                    format!("{} != {}", field_with_table, values_str[0])
                 }
             }
-            FilterOperator::IsEmpty => format!("{} = ''", field_name),
-            FilterOperator::IsNotEmpty => format!("{} != ''", field_name),
+            FilterOperator::IsEmpty => format!("{} = ''", field_with_table),
+            FilterOperator::IsNotEmpty => format!("{} != ''", field_with_table),
             FilterOperator::Like => {
                 let like_op = if case_sensitive.unwrap_or(true) {
                     "LIKE"
@@ -1293,23 +1365,26 @@ impl<T: QueryFilter> SQLConstructor<T> {
                     "ILIKE"
                 };
                 let pattern = self.build_like_pattern(&values_str[0], match_pattern);
-                format!("{} {} {}", field_name, like_op, pattern)
+                if is_plural {
+                    return format!("{}::text {} {}", field_with_table, like_op, pattern);
+                }
+                format!("{} {} {}", field_with_table, like_op, pattern)
             }
             FilterOperator::HasNoValue => {
                 // Check if field is an array by looking for array indicators
-                let is_array_field = field_name.contains("[]")
-                    || field_name.ends_with("_array")
-                    || field_name.ends_with("s");
+                let is_array_field = field_with_table.contains("[]")
+                    || field_with_table.ends_with("_array")
+                    || field_with_table.ends_with("s");
 
                 if is_array_field {
                     // For array fields: check if array length is null or 0
                     format!(
                         "(ARRAY_LENGTH({}, 1) IS NULL OR ARRAY_LENGTH({}, 1) = 0 OR {} IS NULL)",
-                        field_name, field_name, field_name
+                        field_with_table, field_with_table, field_with_table
                     )
                 } else {
                     // For regular fields: check if empty string or null
-                    format!("({} = '' OR {} IS NULL)", field_name, field_name)
+                    format!("({} = '' OR {} IS NULL)", field_with_table, field_with_table)
                 }
             }
         }
@@ -1472,8 +1547,12 @@ impl<T: QueryFilter> SQLConstructor<T> {
                 return String::from("");
             }
 
-            let field_expression =
-                Self::get_field(&self.table, order_by, self.request_body.get_date_format(), self.table.as_str());
+            let field_expression = Self::get_field(
+                &self.table,
+                order_by,
+                self.request_body.get_date_format(),
+                self.table.as_str(),
+            );
 
             // Handle case sensitivity
             let final_field = if self
@@ -1504,10 +1583,15 @@ impl<T: QueryFilter> SQLConstructor<T> {
                     .fields
                     .iter()
                     .map(|field| {
-                        Self::get_field(&self.table, field, self.request_body.get_date_format(), self.table.as_str())
+                        Self::get_field(
+                            &self.table,
+                            field,
+                            self.request_body.get_date_format(),
+                            self.table.as_str(),
+                        )
                     })
                     .collect();
-                
+
                 if is_hypertable(self.table.as_str()) {
                     group_fields.push("timestamp".to_string());
                 }
