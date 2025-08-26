1ed78e47a66bc0eb3374d2c8c919343383ab338b
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2025-08-26 13:44:52 +0800

    working state with build.rs

---
+++ bin/store/build.rs
@@ -0,0 +1,14 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=src/proto/store.proto");
+
+    tonic_build::configure()
+        .build_server(true) // Enable server code (default)
+        .build_client(false) // Enable client code (default)
+        .out_dir("src/generated")
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .message_attribute(".", "#[serde(default)]") // Apply serde(default) only to message types (structs)
+        .compile_protos(&["src/generated/proto/store.proto"], &["src/generated"])?;
+
+    println!("cargo:warning=Successfully compiled proto files");
+    Ok(())
+}
