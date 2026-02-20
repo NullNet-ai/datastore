fea664da67fd606d893c94cbdb397c7b0cdaaa79
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2026-02-20 13:42:06 +0800

    patch

--- bin/store/src/providers/queries/find/queries.rs
+++ bin/store/src/providers/queries/find/queries.rs
@@ -7,11 +7,3 @@ pub struct DynamicResult {
     #[diesel(sql_type = Nullable<Json>)]
     pub row_to_json: Option<serde_json::Value>, // Store as JSON directly
 }
-
-#[derive(QueryableByName, Debug, Serialize)]
-pub struct QueryResult {
-    #[diesel(sql_type = Nullable<Text>)]
-    pub id: Option<String>, // Store as JSON string
-    #[diesel(sql_type = Nullable<Text>)]
-    pub status: Option<String>, // Store as JSON string
-}
